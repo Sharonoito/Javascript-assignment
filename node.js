56a9c8388a272279e10493706242b22f9050ffa7
Sharonoito <oitosharonakoth@gmail.com>
2021-03-09 13:03:58 +0300

    first commit

--- node.js
+++ node.js
@@ -1,10 +1,10 @@
 function fruits(){
     var fruitLists=["Avocado","Grapes","Mangoes","Oranges","Kiwi"];
-    for (let f = 0; f <=fruitLists.length; f++) {
-        
+    for (let f= 0; f<=fruitLists.length; f++) {
+        const element=fruitLists[f];
         console.log(fruitLists[f])
     }
 
 }
 fruits()
-
+    
