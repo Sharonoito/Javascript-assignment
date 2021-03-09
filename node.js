cdf0c816d59764db5a7867e28dc0dd0e723bd6ac
Sharonoito <oitosharonakoth@gmail.com>
2021-03-09 09:18:18 +0300

    first commit

---
+++ node.js
@@ -0,0 +1,10 @@
+function fruits(){
+    var fruitLists=["Avocado","Grapes","Mangoes","Oranges","Kiwi"];
+    for (let f = 0; f <=fruitLists.length; f++) {
+        
+        console.log(fruitLists[f])
+    }
+
+}
+fruits()
+
